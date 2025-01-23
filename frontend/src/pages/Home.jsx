c3bbb74be63ec6712a73d0780ecd33f393473ec4
sudhirKsah <sudhirsahace333@gmail.com>
2025-01-24 02:16:09 +0530

    updated instructions on Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Login in to Admin<br/>Email: admin@gmail.com<br/>Password: Admin123</div>
+    <div>
+      Login in to Admin
+      <br />
+      Email: admin@gmail.com
+      <br />
+      Password: Admin123
+      <br /><br /><br />
+      Please, wait for sometime because the database is hosted on a free server and it takes time to wake up.
+    </div>
   )
 }
 
