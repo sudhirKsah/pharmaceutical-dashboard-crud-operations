ef60145205984014e801f12284471bcf7d9fd9f2
sudhirKsah <sudhirsahace333@gmail.com>
2025-01-23 23:18:33 +0530

    fix: vercel and vite deployment issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+// })
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
